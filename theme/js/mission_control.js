f45b384a8cdd2db1cced58d8dd5b54fc85a3d3b9
Jacob Bednarz <jacob.bednarz@gmail.com>
2014-04-30 08:53:59 +1000

    Dynamically account for tooltip position via a callback

--- theme/js/mission_control.js
+++ theme/js/mission_control.js
@@ -3,7 +3,8 @@
     attach: function() {
       missionControl = window.missionControl || {};
 
-      var index,
+      var self = this,
+          index,
           tipsySelectors = ['.mission-control-hostname', '.mission-control-queue-count'];
 
       // Loop over all the elements that would like to use tooltips and apply
@@ -11,9 +12,28 @@
       for (index = 0; index < tipsySelectors.length; ++index) {
         $(tipsySelectors[index]).tipsy({
           html: true,
-          gravity: 's',
+          gravity: self.gravityPositionCallback,
         });
       }
+    },
+
+    /**
+     * Determine what 'gravity' option to apply to the tooltip when hovering.
+     */
+    gravityPositionCallback: function() {
+      var tooltipWidth = 300,
+          offset = $(this).offset().left,
+          windowWidth = $(window).width();
+
+      if (offset > (windowWidth - tooltipWidth)) {
+        return 'se';
+      }
+
+      if (offset < (tooltipWidth / 2)) {
+        return 'sw';
+      }
+
+      return  's';
     }
   };
 })(jQuery, Drupal, this, this.document);
